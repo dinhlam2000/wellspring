2949972cb700fb6257f6354051643355a6799e11
Lam Tran <lamdtran2000@gmail.com>
2023-11-13 18:32:09 -0800

    added amplify to hosting and updated readme

--- wellspring-take-home/src/shared/services/AppointmentService.tsx
+++ wellspring-take-home/src/shared/services/AppointmentService.tsx
@@ -1,5 +1,7 @@
 export const fetchAppointments = async () => {
+  // const response = await fetch("/assets/appointments.json");
   const response = await fetch("api/appointments");
+
   const data = await response.json();
   return data;
 };
--- wellspring-take-home/src/shared/services/PatientService.tsx
+++ wellspring-take-home/src/shared/services/PatientService.tsx
@@ -1,4 +1,6 @@
 export const fetchPatients = async () => {
+  // const response = await fetch("/assets/patients.json");
+
   const response = await fetch("api/patients");
   const data = await response.json();
   return data;
